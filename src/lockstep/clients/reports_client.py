abf881899be27ae0b37af11a2e400d7cb0700ae6
Ted Spence <tspence@lockstep.io>
2022-03-21 11:31:59 -0700

    Address imports of enums in python

--- src/lockstep/clients/reports_client.py
+++ src/lockstep/clients/reports_client.py
@@ -20,7 +20,6 @@ from lockstep.models.attachmentheaderinfomodel import AttachmentHeaderInfoModel
 from lockstep.models.cashflowreportmodel import CashflowReportModel
 from lockstep.models.dailysalesoutstandingreportmodel import DailySalesOutstandingReportModel
 from lockstep.models.financialreportmodel import FinancialReportModel
-from lockstep.models.reportdepth import ReportDepth
 from lockstep.models.riskratemodel import RiskRateModel
 
 class ReportsClient:
@@ -221,7 +220,7 @@ class ReportsClient:
         else:
             return LockstepResponse(False, result.status_code, None, ErrorResult(**result.json()))
 
-    def income_statement_report(self, startDate: str, endDate: str, columnOption: str, displayDepth: integer) -> LockstepResponse[FinancialReportModel]:
+    def income_statement_report(self, startDate: str, endDate: str, columnOption: str, displayDepth: int) -> LockstepResponse[FinancialReportModel]:
         """
         Generates an Income Statement for the given time range.
 
@@ -239,7 +238,7 @@ class ReportsClient:
             dates Quarterly - a column for every quarter within the
             reporting dates Annually - a column for every year within
             the reporting dates
-        displayDepth : integer
+        displayDepth : int
             The desired row splitting of the report data. Options are as
             follows: 1 - combine all accounts by their category 2 -
             combine all accounts by their subcategory 3 - display all
